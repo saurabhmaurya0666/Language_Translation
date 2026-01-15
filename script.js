6a1769f1a805fb0b53d4f67488d2d4d23b831851
saurabhmaurya0666 <saurabhmaurya445444@gmail.com>
2026-01-15 09:45:51 +0530

    Today  i am add comment in js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function populateDropdown(dropdown, options) {
     dropdown.querySelector("ul").appendChild(li);
   });
 }
-
+//This is use to fetch data from Api
 populateDropdown(inputLanguageDropdown, languages);
 populateDropdown(outputLanguageDropdown, languages);
 
